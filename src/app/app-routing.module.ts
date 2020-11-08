2646671f1ffafa70be54faf78277e978aaaa6e3a
Ben Michaeli <benm@codeoasis.com>
2020-11-08 22:16:15 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./pages/login/login.component";
+import {SearchComponent} from "./pages/search/search.component";
+import {WishlistComponent} from "./pages/wishlist/wishlist.component";
 
 
 const routes: Routes = [
-  // { path: '', component: HomeComponent, pathMatch: 'full' },
-  // { path: 'home', component: HomeComponent },
-  // { path: 'favorites', component: FavoritesComponent },
+  { path: '', component: LoginComponent, pathMatch: 'full' },
+  { path: 'search', component: SearchComponent },
+  { path: 'wishlist', component: WishlistComponent },
 ];
 
 @NgModule({
