9eee59504696df99cb06da515b9dcd9f072fbbf3
Ben Michaeli <benm@codeoasis.com>
2020-11-09 22:41:20 +0200

    clean up service

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {catchError} from "rxjs/operators";
-import {of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,19 +7,14 @@ import {of} from "rxjs";
 export class BooksService {
   private readonly config = {
     BASE_URL: 'https://www.googleapis.com/books/v1/volumes',
-    API_KEY: 'key=AIzaSyABP0xLFrbMJiGUwjH8zhKVHxShG6BSf5w'
+    API_KEY: 'key=AIzaSyABP0xLFrbMJiGUwjH8zhKVHxShG6BSf5w',
+      START_INDEX: 0,
+      MAX_RESULTS: 20
   };
 
   constructor(private http: HttpClient) { }
 
   public searchBooks(query: string) {
-    return this.http.get(`${this.config.BASE_URL}?${this.config.API_KEY}&q=${query}&startIndex=0&maxResults=20`)
-        .pipe(
-            catchError((error) => {
-              // TODO: handle error
-              // this.store.dispatch(openErrorSnackBar({message: 'error message', duration: 3000}));
-              return of(error);
-            })
-        );
+    return this.http.get(`${this.config.BASE_URL}?${this.config.API_KEY}&q=${query}&startIndex=${this.config.START_INDEX}&maxResults=${this.config.MAX_RESULTS}`);
   }
 }
